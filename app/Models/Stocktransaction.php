5109a13a48db262970cc4d62f06d7587f95c582d
najibabdullah <abdullahnajib198@gmail.com>
2025-10-10 13:07:47 +0700

    feat: Define StockTransaction model with fillable attributes and relationships to Product and User

--- app/Models/Stocktransaction.php
+++ app/Models/Stocktransaction.php
@@ -7,25 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class StockTransaction extends Model
 {
+    /** @use HasFactory<\Database\Factories\StockTransactionFactory> */
     use HasFactory;
-
-    protected $fillable = [
-        'product_id',
-        'user_id',
-        'type',
-        'quantity',
-        'date',
-        'status',
-        'notes'
-    ];
-
-    public function product()
-    {
-        return $this->belongsTo(Product::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
